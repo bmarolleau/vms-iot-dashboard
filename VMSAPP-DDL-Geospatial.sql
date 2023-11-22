e0af8ca46a4f7ef08d03ccc5352b97981c773ffe
Benoit Marolleau <benoit.marolleau@fr.ibm.com>
2023-11-22 18:36:31 +0100

    Update VMSAPP-DDL-Geospatial.sql
    Adding trigger with DTAQ for Kafka mobile notifications and Geocoding SQL

--- VMSAPP-DDL-Geospatial.sql
+++ VMSAPP-DDL-Geospatial.sql
@@ -53,6 +53,7 @@ alter table VMSAPP.REFERENCE_LOCATIONS add constraint vmsapp.id PRIMARY KEY(id);
 -- SET REF POINTS LOCATIONS
  INSERT INTO VMSAPP.REFERENCE_LOCATIONS(NAME, CURRENT_LOCATION) values('EMPLOYEE-7249', QSYS2.ST_POINT('point(-92.50365 44.05841)'));
  UPDATE VMSAPP.REFERENCE_LOCATIONS set CURRENT_LOCATION=QSYS2.ST_POINT('point(-92.503 44.0053)')  where id=1; 
+ 
  SELECT ID,QSYS2.ST_ASTEXT(CURRENT_LOCATION) FROM VMSAPP.REFERENCE_LOCATIONS;
 
 CREATE TABLE  VMSAPP.SENSORS  ( 
@@ -69,8 +70,14 @@ CREATE TABLE  VMSAPP.SENSORS  (
          FOREIGN KEY (EVENT_SRC)
          REFERENCES VMSAPP.SENSORS
          ON DELETE  RESTRICT;
-         
--- APP QUERIES examples for computation
+
+          
+ CL: CHGAUT OBJ('/qsys.lib/VMSAPP.lib/SENSORS.file')  USER(acmeair) DTAAUT(*RWX);
+ CL: CHGAUT OBJ('/qsys.lib/VMSAPP.lib/VMSEVENTS.file')  USER(acmeair) DTAAUT(*RWX);
+ CL: CHGAUT OBJ('/qsys.lib/VMSAPP.lib/REFER00001.file')  USER(acmeair) DTAAUT(*RWX);
+ 
+
+-- APP QUERIES examples for computation -----------------------------------------------
 
 SELECT EVT.ID, OBJECTTYPE, DETECTION_SCORE, EVENT_TS, EVENT_SRC, QSYS2.ST_ASTEXT(SENSOR_LOCATION) AS EVENT_LOCATION, 
        QSYS2.ST_ASTEXT(CURRENT_LOCATION) AS REF_LOCATION, ROUND((QSYS2.ST_DISTANCE(CURRENT_LOCATION, SENSOR_LOCATION)),0) AS DISTANCE_M 
@@ -79,8 +86,125 @@ WHERE EVT.EVENT_SRC = SNS.NAME AND SNS.NAME like 'Camera1'
 ORDER BY EVT.ID DESC;
 
 INSERT INTO VMSAPP.VMSEVENTS(OBJECTTYPE, DETECTION_SCORE, EVENT_TS, EVENT_SRC) values('person','91.14156365394592',TIMESTAMP_FORMAT('2023-06-01 11:37:22.897', 'YYYY-MM-DD HH24:MI:SS.FF3'),'Camera1');
-          
- CL: CHGAUT OBJ('/qsys.lib/VMSAPP.lib/SENSORS.file')  USER(acmeair) DTAAUT(*RWX);
- CL: CHGAUT OBJ('/qsys.lib/VMSAPP.lib/VMSEVENTS.file')  USER(acmeair) DTAAUT(*RWX);
- CL: CHGAUT OBJ('/qsys.lib/VMSAPP.lib/REFER00001.file')  USER(acmeair) DTAAUT(*RWX);
 
+ select * from  QSYS2.ST_SPATIAL_REFERENCE_SYSTEMS ;
+ 
+ CREATE VARIABLE ADDRESS VARCHAR(100);
+ 
+ --GEOCODING REQUEST USA
+ SET ADDRESS = URL_ENCODE('4 3rd St SW, Rochester, MN 55902');
+ Values ADDRESS; 
+ 
+ SELECT * FROM TABLE(QSYS2.HTTP_GET_VERBOSE('https://geocoding.geo.census.gov/geocoder/locations/onelineaddress','')); 
+ select QSYS2.ST_ASTEXT(st_point(longitude, latitude)) as thai_pop_geo from json_table(QSYS2.HTTP_GET(
+'https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address='
+ 
+ concat ADDRESS concat '&benchmark=2020&format=json', 
+ '{"header":
+ "User-Agent,Benoit","sslCertificateStoreFile":"/home/javaTrustStore/fromJava.KDB"}'
+ ),
+ 'lax $.result.addressMatches[0].coordinates'
+ 
+ columns(
+longitude varchar(100) path 'lax $.x',
+latitude varchar(100) path 'lax $.y'));
+ 
+ ----Geocoding API USA https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address=4+3rd+St+SW%2C+Rochester%2C+MN+55902&benchmark=2020&format=json
+ 
+ --GEOCODING REQUEST FRANCE
+  SET ADDRESS = URL_ENCODE('1 rue vieille poste, 34000 Montpellier');
+ Values ADDRESS;
+     
+  select  longitude, latitude  from json_table(QSYS2.HTTP_GET(
+'https://api-adresse.data.gouv.fr/search?q='
+concat ADDRESS, 
+  '{"header":"User-Agent,Benoit","sslCertificateStoreFile":"/home/javaTrustStore/fromJava.KDB"}'),
+  'lax $.features[0].geometry'
+ 
+ columns(
+longitude varchar(100) path 'lax $.coordinates[0]',
+latitude varchar(100) path 'lax $.coordinates[1]'));
+  
+  select  QSYS2.ST_ASTEXT(st_point(longitude, latitude)) as IBMMOP from json_table(QSYS2.HTTP_GET(
+'https://api-adresse.data.gouv.fr/search?q='
+concat ADDRESS, 
+  '{"header":"User-Agent,Benoit","sslCertificateStoreFile":"/home/javaTrustStore/fromJava.KDB"}'),
+  'lax $.features[0].geometry'
+ 
+ columns(
+longitude varchar(100) path 'lax $.coordinates[0]',
+latitude varchar(100) path 'lax $.coordinates[1]'));
+  
+ -- Check result here https://geofree.fr/gf/coordinateconv.asp#listSys
+ -- Geocoding API France:  https://api-adresse.data.gouv.fr/search/?q=8+bd+du+port&x=-92.503&y=44.0053
+ -- FR docs http://www.postgis.fr/chrome/site/docs/workshop-foss4g/doc/geometries.html
+ 
+  /************************ TRIGGER to DTAQ to CAMEL+Kafka  ****************************/
+
+create or replace variable VMSAPP.DQ_EVENT clob(64000) ccsid 1208;
+
+--  description: Create EVENT trigger 
+CREATE OR REPLACE TRIGGER VMSAPP.EVENT1_TRIGGER 
+	AFTER INSERT OR DELETE OR UPDATE ON VMSAPP.vmsevents 
+	REFERENCING OLD AS O 
+	NEW AS N 
+	FOR EACH ROW 
+	MODE DB2SQL 
+	SET OPTION  ALWBLK = *ALLREAD , 
+	ALWCPYDTA = *OPTIMIZE , 
+	COMMIT = *ALL , 
+	DECRESULT = (31, 31, 00) , 
+	DFTRDBCOL = VMSAPP , 
+	DYNDFTCOL = *NO , 
+	DYNUSRPRF = *USER , 
+	SRTSEQ = *HEX   
+	WHEN ( INSERTING OR UPDATING OR DELETING )
+ BEGIN ATOMIC 
+DECLARE OPERATION VARCHAR ( 10 ) FOR SBCS DATA ; 
+IF INSERTING THEN 
+SET SQLP_L3.OPERATION = 'INSERT' ; 
+END IF ; 
+IF DELETING THEN 
+SET SQLP_L3.OPERATION = 'DELETE' ; 
+END IF ; 
+IF UPDATING THEN 
+SET SQLP_L3.OPERATION = 'UPDATE' ; 
+END IF ; 
+IF ( INSERTING OR UPDATING ) THEN 
+SET VMSAPP.DQ_EVENT = JSON_OBJECT ( 
+KEY 'table' VALUE 'vmsevents' , 
+KEY 'operation' VALUE SQLP_L3.OPERATION , 
+KEY 'row' VALUE JSON_OBJECT ( 
+KEY 'id' VALUE N.ID , 
+KEY 'objecttype' VALUE N.OBJECTTYPE , 
+KEY 'detection_score' VALUE N.DETECTION_SCORE , 
+KEY 'event_ts' VALUE N.EVENT_TS , 
+KEY 'event_src' VALUE N.EVENT_SRC  
+) 
+) ; 
+ELSE SET VMSAPP.DQ_EVENT = JSON_OBJECT ( 
+KEY 'table' VALUE 'vmsevents' , 
+KEY 'operation' VALUE SQLP_L3.OPERATION , 
+KEY 'row' VALUE JSON_OBJECT ( 
+KEY 'id' VALUE N.ID , 
+KEY 'objecttype' VALUE N.OBJECTTYPE , 
+KEY 'detection_score' VALUE N.DETECTION_SCORE , 
+KEY 'event_ts' VALUE N.EVENT_TS , 
+KEY 'event_src' VALUE N.EVENT_SRC 
+) 
+) ; 
+END IF ; 
+CALL QSYS2.SEND_DATA_QUEUE_UTF8 ( 
+MESSAGE_DATA => VMSAPP.DQ_EVENT , 
+DATA_QUEUE => 'EVENTSDQ' , 
+DATA_QUEUE_LIBRARY => 'VMSAPP' 
+) ; 
+END  ; 
+
+CL: CRTDTAQ DTAQ(VMSAPP/EVENTSDQ) MAXLEN(64512) SEQ(*LIFO) SIZE(*MAX2GB);
+
+SELECT * FROM TABLE(QSYS2.DATA_QUEUE_ENTRIES(
+           DATA_QUEUE => 'EVENTSDQ',
+           DATA_QUEUE_LIBRARY => 'VMSAPP')) ;
+  
+ SELECT * FROM VMSAPP.VMSEVENTS ORDER BY ID DESC;
